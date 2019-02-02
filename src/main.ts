420901213fa9da016731208a162aa3b3532a5025
Bernix01 <gbernal096@gmail.com>
2019-02-02 11:06:50 -0500

    SSR

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
+import 'neuets';
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+export default ({ router, store }: any) => {
+  return new Vue({
+    router,
+    store,
+    render: h => h(App)
+  });
+}
